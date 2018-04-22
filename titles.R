276b16861f18faffeddf43edcefdc1ef7304b126
calmesam01 <sanyamsareen01@git.com>
2018-04-23 01:52:46 +0530

    get_titles

---
+++ titles.R
@@ -0,0 +1,8 @@
+# The data set shakespeare in available in the workspace.
+
+shakespeare
+
+# Pipe the shakespeare data frame to the next line shakespeare %>% 
+# Use count to find out how many titles/types there are
+
+count(title, type)
