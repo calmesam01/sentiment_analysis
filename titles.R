ad7c418f7d8513e51b352e0a166eb089d6638e01
Sanyam Sareen <sanyamsareen01@gmail.com>
2018-04-25 00:07:26 +0530

    Delete titles.R

--- titles.R
+++
@@ -1,8 +0,0 @@
-# The data set shakespeare in available in the workspace.
-
-shakespeare
-
-# Pipe the shakespeare data frame to the next line shakespeare %>% 
-# Use count to find out how many titles/types there are
-
-count(title, type)
